6df8124d41a4de1e4cc8e325f7402ac5d4f459fe
31VS1 <121323408+31VS1@users.noreply.github.com>
2023-02-25 23:03:48 -0600

    test

--- 6872274481.lua
+++ 6872274481.lua
@@ -7185,10 +7185,10 @@ runcode(function()
 						allowed = ((lplr.Character:GetAttribute("InflatedBalloons") and lplr.Character:GetAttribute("InflatedBalloons") > 0) or matchState == 2 or megacheck) and 1 or 0
 						local mass = (entityLibrary.character.HumanoidRootPart:GetMass() - 1.4) * (delta * 100)
 						local realflyspeed = flyspeed.Value
-						mass = mass + (allowed > 0 and 10 or 0.03) * (flytog and -0 or 0)
+						mass = mass + (allowed > 0 and 10 or  2.3) * (flytog and -1 or 1)
 						if flytogtick <= tick() then
 							flytog = not flytog
-							flytogtick = tick() + (allowed > 0 and 0.2 or 0.2)
+							flytogtick = tick() + (allowed > 0 and 0.2 or 0.06)
 						end
 						if flyacprogressbarframe then
 							flyacprogressbarframe.Visible = allowed <= 0
